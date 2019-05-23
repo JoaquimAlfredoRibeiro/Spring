d1c71648c34ecf665a7133766df8cad7d902ad34
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2019-05-23 17:04:02 +0100

    Initial commit

---
+++ src/main/java/pt/home/spring5webapp/Author.java
@@ -0,0 +1,27 @@
+package pt.home.spring5webapp;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+}
